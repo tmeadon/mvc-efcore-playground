9d1afc5ecaee639d7c9706d728eb4baf654eac4b
Tom Meadon <tom.meadon@newsignature.com>
2022-02-07 17:26:03 +0000

    initial commit

---
+++ TestApp/Models/Course.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestApp.Models;
+
+public class Course
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int CourseID { get; set; }
+    public string Title { get; set; } = "";
+    public int Credits { get; set; }
+    public ICollection<Enrollment> Enrollments { get; set; } 
+}
