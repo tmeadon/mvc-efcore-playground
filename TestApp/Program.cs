e2270fba9e47212ab5f194f39e0905956f10483b
Tom Meadon <tom@meadon.me>
2022-02-08 14:23:01 +0000

    add migrations

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -31,7 +31,6 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<SchoolContext>();
-    context.Database.EnsureCreated();
     DbInitializer.Initialize(context);
 }
 
