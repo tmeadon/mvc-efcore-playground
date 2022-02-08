d38d3a101bf93ce017892c3acf7b2d8a2665d6e8
Tom Meadon <tom@meadon.me>
2022-02-08 14:51:11 +0000

    update student model and add migration

--- TestApp/Models/Student.cs
+++ TestApp/Models/Student.cs
@@ -1,12 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TestApp.Models;
 
-public class Student 
+public class Student
 {
     public int ID { get; set; }
-    public string LastName { get; set; } = "";
-    public string FirstMidName { get; set; } = "";
+
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+    
+    [Required]
+    [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+    [Column("FirstName")]
+    [Display(Name = "First Name")]
+    public string FirstMidName { get; set; }
+
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    [Display(Name = "Enrollment Date")]
     public DateTime EnrollmentDate { get; set; }
+    
+    [Display(Name = "Full Name")]
+    public string FullName { get => $"{LastName}, {FirstMidName}"; }
 
-    public virtual ICollection<Enrollment> Enrollments { get; set; }
-
+    public ICollection<Enrollment> Enrollments { get; set; }
 }
