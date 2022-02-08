cd717d62eb83e5275851fcbb66c2ab25d8103fc8
Tom Meadon <tom@meadon.me>
2022-02-08 16:46:37 +0000

    re-work data model

--- TestApp/Models/Course.cs
+++ TestApp/Models/Course.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TestApp.Models;
@@ -5,8 +6,16 @@ namespace TestApp.Models;
 public class Course
 {
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    [Display(Name = "Number")]
     public int CourseID { get; set; }
+
+    [StringLength(50, MinimumLength = 3)]
     public string Title { get; set; } = "";
+    
+    [Range(0, 5)]
     public int Credits { get; set; }
+
+    public Department Department { get; set; }
     public ICollection<Enrollment> Enrollments { get; set; } 
+    public ICollection<Instructor> Instructors { get; set; }
 }
